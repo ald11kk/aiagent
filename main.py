8ec3e0a748699b0e4935c1b3ec428e06d89b1f39
ald11kk <221241@astanait.edu.kz>
2026-03-20 14:19:25 +0500

    add CLI input via argparse for user prompt

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def main():
-    import os
-    from dotenv import load_dotenv
-    from google import genai
+import argparse
+import os
+from dotenv import load_dotenv
+from google import genai
 
+def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
@@ -10,17 +11,19 @@ def main():
         return
     client = genai.Client(api_key=api_key)
 
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser = argparse.ArgumentParser(description="ChatBot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents = prompt
+        contents = args.user_prompt
     )
 
     if response.usage_metadata == None:
         raise RuntimeError("Response does not contain usage metadata.")
 
-    print(f"User prompt: {prompt}")
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}")
