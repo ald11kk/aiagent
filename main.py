f4812cf22b9bcc43c4814402356fe42bbf95d748
ald11kk <221241@astanait.edu.kz>
2026-03-20 13:56:08 +0500

    add Gemini API integration with hardcoded prompt and output

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 def main():
-    print("Hello from aiagent!")
+    import os
+    from dotenv import load_dotenv
+    from google import genai
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        print("GEMINI_API_KEY not found in environment variables.")
+        return
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    
+    print(response.text)
 
 
 if __name__ == "__main__":
