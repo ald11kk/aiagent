b283202655363591caf613c12ba831f32f2b4fcb
ald11kk <221241@astanait.edu.kz>
2026-03-20 14:30:14 +0500

    add --verbose flag for token usage and prompt output

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -13,19 +14,23 @@ def main():
 
     parser = argparse.ArgumentParser(description="ChatBot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents = args.user_prompt
+        contents = messages
     )
 
     if response.usage_metadata == None:
         raise RuntimeError("Response does not contain usage metadata.")
 
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}")
 
 
