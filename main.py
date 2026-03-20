f81585d696d9793f70043c8f342770bf5da42ca8
ald11kk <221241@astanait.edu.kz>
2026-03-20 14:03:57 +0500

    add token usage tracking for Gemini API responses

--- main.py
+++ main.py
@@ -10,12 +10,20 @@ def main():
         return
     client = genai.Client(api_key=api_key)
 
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents = prompt
     )
-    
-    print(response.text)
+
+    if response.usage_metadata == None:
+        raise RuntimeError("Response does not contain usage metadata.")
+
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
